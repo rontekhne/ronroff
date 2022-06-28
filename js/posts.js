0c9f19e392f1cab2d5483974c84042e4cad0986c
Ron <79697100+rontekhne@users.noreply.github.com>
2022-06-28 11:17:46 -0300

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,13 +1,26 @@
 (async() => {
+    let id = [];
+
     for (let i = 1; i <= 3; i++) {
         const url = `https://raw.githubusercontent.com/rontekhne/ronroffposts/main/post${i}`;
         const response = await fetch(url);
         const data = await response.text();
-        
+
+        let p = preview(data); // extrai uma amostra
+        p += "..."; 
+
         const div = document.createElement("div");
-        div.innerHTML = data;
+        div.innerHTML = p;
 
         const post = document.querySelector("#posts");
         post.appendChild(div);
     }
 })()
+
+function preview(str) {
+    if (str.length > 250) {
+        return str.slice(0, 250);
+    }
+
+    return str;
+}
