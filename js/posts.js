13efd71f3f3c267bfc41ce047be99ac008d3b25c
Ron <79697100+rontekhne@users.noreply.github.com>
2022-06-29 08:31:25 -0300

    Update posts.js
    Sending data via GET

--- js/posts.js
+++ js/posts.js
@@ -12,7 +12,7 @@
         const section = document.createElement("section");
         section.innerHTML = 
             p + 
-            `<br><button type="submit">Leia mais...</button>;`;
+            `<br><button onclick="openPost(${i})" type="submit">Leia mais...</button>;`;
 
         const post = document.querySelector("#posts");
         post.appendChild(section);
@@ -25,4 +25,8 @@ function preview(str) {
     }
 
     return str;
+}
+
+function openPost(index) {
+    window.location.href = `../post.html?id=${index}`;
 }
