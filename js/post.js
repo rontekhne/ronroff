9f8f506b081ed26fcb677964f75a8927b8c1287e
Ron <79697100+rontekhne@users.noreply.github.com>
2022-06-29 09:26:39 -0300

    Update post.js

--- js/post.js
+++ js/post.js
@@ -3,8 +3,6 @@
     const urlParams = new URLSearchParams(currentUrl);
     const id = urlParams.get('id');
     console.log(id);
-    
-    history.pushState({}, null, "https://rontekhne.github.io/post.html");
 
     const url = `https://raw.githubusercontent.com/rontekhne/ronroffposts/main/post${id}`;
     const response = await fetch(url);
