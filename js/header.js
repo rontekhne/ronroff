1b2b439a589ae0b6b8d0277ec9e70d94376b10ac
ron <rontekhne@users.noreply.github.com>
2022-07-02 12:53:02 -0300

    added some style

--- js/header.js
+++ js/header.js
@@ -4,7 +4,15 @@ function header()
     <header>
         <nav>
             <ul>
-                <li class="logo"><a href="">RonRoff</a></li>
+                <li class="logo"><a href="">
+                    <span style="color:#F43545">R</span>
+                    <span style="color:#FA8901">o</span>
+                    <span style="color:#FAD717">n</span>
+                    <span style="color:#00BA71">R</span>
+                    <span style="color:#00C2DE">o</span>
+                    <span style="color:#00418D">f</span>
+                    <span style="color:#5F2879">f</span>
+                </a></li>
                 <li class="menu-link"><a href="">sobre</a></li>
                 <li class="menu-link"><a href="">contato</a></li>
             </ul>
