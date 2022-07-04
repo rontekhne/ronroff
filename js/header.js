167aee74a4447e4a0dc371f16f4d92118b5786a6
Ron <79697100+rontekhne@users.noreply.github.com>
2022-07-04 09:01:52 -0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -4,8 +4,10 @@ function header()
     <header>
         <nav>
             <ul>
-                <li class="logo"><a href="https://rontekhne.github.io/ronroff">RonRoff</a></li>
-                <div class="menu-link">
+                <div class="logo">
+                    <li><a href="https://rontekhne.github.io/ronroff">RonRoff</a></li>
+                </div>
+                <div class="menu-link">    
                     <li><a href="https://rontekhne.github.io/ronroff/about.html">sobre</a></li>
                     <li><a href="https://rontekhne.github.io/ronroff/contact.html">contato</a></li>
                 </div>
